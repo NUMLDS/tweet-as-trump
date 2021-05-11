683d5777647f968043a3303e2b006696707fc7fa
Dian Yu <dianyu2021@u.northwestern.edu>
2021-05-10 21:57:42 -0500

    Remove unnecessary functionalities

--- src/add_tweets.py
+++ src/add_tweets.py
@@ -1,10 +1,8 @@
 import logging.config
 
 import sqlalchemy
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, BigInteger, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
@@ -27,64 +25,12 @@ class Tweets(Base):
 
 def create_db(engine_string):
     """Create database from provided engine string.
-
     Args:
         engine_string (str): Engine string.
-
     Returns:
         None
-
     """
     engine = sqlalchemy.create_engine(engine_string)
 
     Base.metadata.create_all(engine)
     logger.info("Database created.")
-
-
-class TweetManager:
-
-    def __init__(self, app=None, engine_string=None):
-        """Initialize a TweetManager object.
-
-        Args:
-            app (Flask): Flask app.
-            engine_string (str): Engine string.
-
-        """
-        if app:
-            self.db = SQLAlchemy(app)
-            self.session = self.db.session
-        elif engine_string:
-            engine = sqlalchemy.create_engine(engine_string)
-            Session = sessionmaker(bind=engine)
-            self.session = Session()
-        else:
-            raise ValueError("Need either an engine string or a Flask app to initialize")
-
-    def close(self):
-        """Close session.
-
-        Returns:
-            None
-
-        """
-        self.session.close()
-
-    def add_tweet(self, id, date, content, retweets):
-        """Seed an existing database with additional tweets.
-
-        Args:
-            id (str): ID of the tweet
-            date (str): Date of the tweet
-            content (str): Content of the tweet
-            retweets (str): Number of retweets as a String
-
-        Returns:
-            None
-
-        """
-        session = self.session
-        content = Tweets(id=id, date=date, content=content, retweets=retweets)
-        session.add(content)
-        session.commit()
-        logger.info("Tweet with id %s, added to database", id)
